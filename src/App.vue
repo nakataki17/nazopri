b9dbfae77dbb27ff5eab5b8524e25882dd99a17a
NNakata <detteiu.bloom@gmail.com>
2022-03-01 18:15:02 +0900

    ナビゲーションバー

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <NavigationBar/>
-  <img class="m-auto" alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 
   <CameraArea/>
@@ -23,13 +22,4 @@ export default {
 
 <style>
 
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
