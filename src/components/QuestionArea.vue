70503e924f9b84c84306f49a7eaa9a8b28ba1bff
NNakata <detteiu.bloom@gmail.com>
2022-03-24 20:13:46 +0900

    タイマーデザイン

--- src/components/QuestionArea.vue
+++ src/components/QuestionArea.vue
@@ -2,10 +2,14 @@
     <div class="w-5/6 h-full mx-auto text-center">
       <img class=" text-center  mx-auto my-auto"  v-bind:src="imgSrc(course,qno)" alt="">
     </div>
+    <span class=" countdown font-mono text-6xl flex justify-center" v-if="time>=-1">
+      <span v-bind:style="'--value:'+time+';'" v-bind:class="{'text-red-500':time<=5}" ></span>
+    </span>
+    <span class="text-red-500 font-title text-3xl flex justify-center" v-else>Time's up!</span>
 </template>
 
 <script>
-import { defineComponent, onMounted, } from "vue";
+import { defineComponent, onMounted, ref} from "vue";
 
 export default defineComponent({
   name: "QuestionArea",
@@ -16,6 +20,10 @@ export default defineComponent({
   },
 
   setup(props) {
+    //こいつは常に一意のはず
+    const timerID = ref()
+    const time = ref(20)
+
     onMounted(() => {
 //        document.addEventListener("keydown",props.keyboardPress)
       console.log("mounted qArea")
@@ -25,9 +33,28 @@ export default defineComponent({
       console.log("@/assets/"+props.course+"/"+props.qno+".png")
       return require("@/assets/"+props.course+"/"+props.qno+".png")
     }
-  
+
+    const startTimer = (length) => {
+      time.value = length
+      timerID.value = setInterval(()=>{
+        if(time.value > -1 ){
+          time.value--
+        }else{
+          time.value--
+          clearInterval(timerID.value)
+        }
+      },1000)
+    }
+
+
+
+    startTimer(20)
+
     return{
-     imgSrc, 
+      timerID,
+      imgSrc, 
+      startTimer,
+      time
     }
   },
 })
