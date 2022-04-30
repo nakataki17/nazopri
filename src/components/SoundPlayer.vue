fba7ad379510b26a75b2642e4d0aca692501d09a
NNakata <detteiu.bloom@gmail.com>
2022-04-30 22:41:49 +0900

    音声埋め込み

--- src/components/SoundPlayer.vue
+++ src/components/SoundPlayer.vue
@@ -3,7 +3,7 @@
 <script>
 import { defineComponent, watchEffect,} from "vue";
 import voiceData from "../assets/voiceData/voices.json";
-
+import questionData from "../assets/questionData/questions.json";
 import { Howl,  } from 'howler';
 
 
@@ -31,16 +31,13 @@ export default defineComponent({
       //courseを指定してそこの音声読み込みを行う
       Object.keys(voiceData[course]).forEach((imgInd)=>{
         Object.keys(voiceData[course][imgInd]).forEach((time)=>{
-          console.log(voiceData[course][imgInd])
           const fileName = voiceData[course][imgInd][time]["ファイル名"]
-          console.log(fileName)
           if(audioList[fileName] === null){
           const sound = new Howl({
           src: "https://fusma.github.io/nazopri_Sound/" + fileName
           })
           audioList[fileName] = sound
           console.log("Successfully loaded "+fileName )
-          console.log(audioList[fileName])
           }
         })
       })   
@@ -53,11 +50,19 @@ export default defineComponent({
     })
 
     watchEffect(()=>{
-      if(props.time in voiceData[props.course][props.imageInd]){
+      if(props.time == questionData[props.course][props.imageInd]["制限時間"] && -1 in voiceData[props.course][props.imageInd] ){
+        const fileName = voiceData[props.course][props.imageInd][-1]["ファイル名"]
+      setTimeout(()=>{
+        audioList[fileName].play()
+      },500)
+      }else{
+      if(props.time in voiceData[props.course][props.imageInd] && props.time > -1){
       const fileName = voiceData[props.course][props.imageInd][props.time]["ファイル名"]
       console.log(audioList[fileName])
-      audioList[fileName].play()
-      }
+      setTimeout(()=>{
+        audioList[fileName].play()
+      },500)
+      }}
 
     })
 
