919bb8e7c100eec50bcda2724a7b74bd49f3457e
NNakata <detteiu.bloom@gmail.com>
2022-05-05 01:23:58 +0900

    画像の埋め込み

--- src/assets/questionData/convertCSVtoJSON.py
+++ src/assets/questionData/convertCSVtoJSON.py
@@ -2,18 +2,18 @@ import csv
 import json
 
 jsonDic = dict()
-with open("voices.csv",encoding="UTF-8") as f:
+with open("questions.csv",encoding="UTF-8") as f:
   reader = csv.reader(f)
   for n,row in enumerate(reader):
     if n == 0:
       continue
     courseName,imgInd,questionInd,time,fileName = row
-    if courseName=="コース名":continue
+    if "コース名" in courseName:continue
     if courseName not in jsonDic:
       jsonDic[courseName] = dict()
     if imgInd not in jsonDic[courseName]:
       jsonDic[courseName][imgInd] = dict()
-    jsonDic[courseName][imgInd][time] = {"問題番号":questionInd, "ファイル名":fileName,}
+    jsonDic[courseName][imgInd] = {"問題番号":questionInd, "制限時間":time, "ファイル名":fileName,}
     print(fileName)
 
 enc = json.dumps(jsonDic,indent=2,ensure_ascii=False)
